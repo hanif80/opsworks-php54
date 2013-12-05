cc4cd2c33a5bff715ed91180032fa70763ff9203
hanif80 <hanif80ster@gmail.com>
2013-12-05 14:25:25 +0600

    Update dir-creation.rb

--- mod_php54/attributes/dir-creation.rb
+++ mod_php54/attributes/dir-creation.rb
@@ -1,5 +1,5 @@
 %w{profile_picture lead_images client_images repair_images purchase_images wish_images sales_images}.each do |dir|
-  directory "/APPLICATION PATH/images/#{dir}" do
+  directory "/var/www/html/images/#{dir}" do
     mode 00775
     owner "root"
     group "root"
